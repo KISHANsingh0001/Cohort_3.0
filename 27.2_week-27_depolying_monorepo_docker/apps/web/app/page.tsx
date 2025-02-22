382c60e5218281b184f74d458b98a2ac8699d7d9
Kishan <kishansinghthakur27@gmail.com>
2025-02-22 21:36:59 +0530

    added docker files

--- 27.2_week-27_depolying_monorepo_docker/apps/web/app/page.tsx
+++ 27.2_week-27_depolying_monorepo_docker/apps/web/app/page.tsx
@@ -1,22 +1,27 @@
 import {client} from 'db/client'
 
 export default async function Home() {
+
   const users = await client.user.findMany();
   return (
     <div style={{margin:10}}>
-     <p style={{textAlign:'center'}}><h1>Users</h1></p>
+     <p style={{textAlign:'center' , fontFamily:'sans-serif'}}><h1>Users Information</h1></p>
      {users.map(({id , username , password})=>{
        return (
-         <div style={{}}>
+         <div style={{margin:20}}>
           <div style={{margin:10}}><h3>Id:</h3>{id}</div>
           <div style={{margin:10}}><h3>Username:</h3>{username}</div>
           <div style={{margin:10}}><h3>Password:</h3>{password}</div>
           <hr />
-         </div>
-         
-       )
-     })}
-    </div>
-    
+         </div>)
+        {/* <div>{JSON.stringify(users)}</div> */}
+      })}
+    </div>   
   );
 }
+
+export const revalidate = 10 
+// // revalidate every 60 seconds
+// or
+// Incremental site generation
+// export const dynamic = 'force-dynamic'
